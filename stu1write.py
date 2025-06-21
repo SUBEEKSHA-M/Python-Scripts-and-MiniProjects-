340a7dfa3cc0299f0c29dca1da2739c5d5f0ff8a
SUBEEKSHA-M <subee22it@gmail.com>
2025-06-21 10:08:56 +0530

    add DictWriter

--- stu1write.py
+++ stu1write.py
@@ -5,3 +5,9 @@ home = input("where's your home?")
 with open("stu1write.csv", "a") as file:
     writer = csv.writer(file)
     writer.writerow([name, home])
+
+#dictwriter
+
+with open("stu1write.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name,"home": home})
